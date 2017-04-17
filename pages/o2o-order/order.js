4ee8ef349c250b266654b86a79fb3da2f442f924
MinJianFu <1042593640@qq.com>
2017-04-17 23:20:43 +0800

    增加订单、订单详情

--- pages/o2o-order/order.js
+++ pages/o2o-order/order.js
@@ -1,11 +1,18 @@
 Page({
     data:{
         header:['进心中','已完成','已取消'],
-        state:0
+        state:0,
+        a:{
+            a:1,
+            b:2
+        }
     },
     changestate:function(e){
         this.setData({
             state:e.currentTarget.dataset.key
         })
+    },
+    onReady:function(){
+
     }
 })
